74ba49cb223abfeb599d722a3189bb620beba320
islaybears <marcndanny@gmail.com>
2017-11-01 02:34:34 -0500

    cleaner merged menubar onto header file

--- app/appRoutes.js
+++ app/appRoutes.js
@@ -1,10 +1,22 @@
-app = angular.module('inventory', ['ngRoute'])
-      .config(function($locationProvider, $routeProvider){
-        $routeProvider
-        .when('/',{templateUrl: 'app/pages/home/homeView.html', controller: 'homeCtrl'})
-        .when('/bullet-hell',{templateUrl: 'app/pages/bulletHell/bulletHellView.html', controller: 'bulletHellCtrl'})
-        .when('/connect-4', {templateUrl: 'app/pages/connect4/connect4View.html', controller: 'connect4Ctrl'})
-        .otherwise({redirectTo:'/'});
+var app = angular.module('inventory', ['ngRoute']);
 
-        $locationProvider.html5Mode(true);
-      });
+app.config(function($locationProvider, $routeProvider){
+  $routeProvider
+    .when('/', {
+      templateUrl: 'app/pages/home/homeView.html',
+      controller: 'homeCtrl'
+    })
+    .when('/bullet-hell', {
+      templateUrl: 'app/pages/bulletHell/bulletHellView.html',
+      controller: 'bulletHellCtrl'
+    })
+    .when('/connect-4', {
+      templateUrl: 'app/pages/connect4/connect4View.html',
+      controller: 'connect4Ctrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+    
+  $locationProvider.html5Mode(true);
+});
